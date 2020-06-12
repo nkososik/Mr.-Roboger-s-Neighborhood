18d1681853020805e805b07f265c86e664107447
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-12 15:18:38 -0700

    adjust spacing in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function rangeConverter(numberInput) {
   for (let i = 0; i <= numberInput; i++) {
     range.push([i].toString()); 
   };
+
   let result = range.map(function(element) {
     if (element.includes('3')) {
       return element = '"Won\'t you be my neighbor?"';
@@ -21,16 +22,14 @@ function rangeConverter(numberInput) {
 $(document).ready(function() {
   $('form#userNumber').submit(function(event) {
     event.preventDefault();
-    const numberInput = parseInt($('input[name=number]').val());
 
+    const numberInput = parseInt($('input[name=number]').val());
     if (!numberInput || numberInput < 0) {
       alert("Enter a friendly positive number wouldn't ya neighbor?");
       return;
     }
 
     let results = rangeConverter(numberInput);
-    console.log(typeof results);
-
     $('#rangeResult').show();
     $('#userInput').text(numberInput);
     $('#rangeOutput').text(results);
